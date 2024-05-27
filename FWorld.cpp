7ccc423881250e0c801c96f0f5bb066a8381b9e5
ych <korea940414@naver.com>
2024-05-27 14:47:26 +0900

    14:30 클래스

---
+++ FWorld.cpp
@@ -0,0 +1,15 @@
+#include "FWorld.h"
+#include <iostream>
+
+using namespace std;
+
+FWorld::FWorld()
+{
+	cout << "FWorld »ý¼º" << endl;
+
+}
+
+FWorld::~FWorld()
+{
+	cout << "FWorld ÆÄ±«" << endl;
+}
